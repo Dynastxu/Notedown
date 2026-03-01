e3741c272b0a611e195c78702bc635d06bdcf3c1
dynastxu <151742029+ETOofficial@users.noreply.github.com>
2026-03-01 15:48:08 +0800

    feat(drawer): 优化抽屉导航手势控制逻辑
    - 添加了导航控制器当前堆栈状态监听
    - 引入了当前路由获取功能
    - 实现了仅在主页启用抽屉手势的功能
    - 修复了非主页页面抽屉手势误触发问题

--- app/src/main/java/com/dynastxu/notedown/MainActivity.kt
+++ app/src/main/java/com/dynastxu/notedown/MainActivity.kt
@@ -12,10 +12,12 @@ import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.dynastxu.notedown.models.data.Route
 import com.dynastxu.notedown.models.view.MainViewModel
@@ -41,6 +43,8 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
                 val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
                 val scope = rememberCoroutineScope()
+                val currentBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentRoute = currentBackStackEntry?.destination?.route
 
                 // 抽屉
                 ModalNavigationDrawer(
@@ -50,7 +54,7 @@ class MainActivity : ComponentActivity() {
                             AppDrawerContent(navController, drawerState, scope, viewModel)
                         }
                     },
-                    gesturesEnabled = true
+                    gesturesEnabled = currentRoute == Route.HOME
                 ) {
                     // 外层 Scaffold，包含动态顶部栏
                     Scaffold(
